745ef0ac0a6020a0e6d29c58c64176041c6facbe
Tiziano Dallavilla <tiziano.dallavilla@gmail.com>
2023-11-06 16:34:28 +0100

    Created a class to store data and one to manage data

--- lib/ml_feature_prediction.py
+++ lib/ml_feature_prediction.py
@@ -1,1 +1,19 @@
+from dataclasses import dataclass
 import pandas as pd
+
+
+# Class that contains data
+@dataclass
+class dataObj:
+	path: str
+	data: pd.DataFrame()
+	type_of_data: str
+
+
+class dataManager:
+	def createDataObject(path, type_of_data, sep=','):
+		if type not in ['measurement', 'clinical']:
+			print('Type can only be \'measurement\' or \'clinical\'')
+		else:
+			data = pd.read_csv(path, sep=sep)
+	return dataObj(path, data, type_of_data)
