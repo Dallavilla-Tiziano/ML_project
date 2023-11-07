294a54988cfbade3c0c03e882c24c2ea907f0c81
Tiziano Dallavilla <tiziano.dallavilla@gmail.com>
2023-11-07 17:16:34 +0100

    -modified createDataObject to avoid overwriting a dataObj by giving the same name to two different dataObj -modified createDataObject to allow to set an index during dataObj creation -modified dataObjectJoin to allow join only if dataObj indexes are different than pd.RangeIndex

--- lib/ml_feature_prediction.py
+++ lib/ml_feature_prediction.py
@@ -13,6 +13,7 @@ class dataObj:
 	data: pd.DataFrame()
 	type_of_data: str
 	description: str = ''
+	is_data_index_set: bool = False
 
 
 class dataManager:
@@ -20,19 +21,35 @@ class dataManager:
 		self.data_container = {}
 		self.dataManager_name = name
 
-	def createDataObject(self, path, name, type_of_data, description='', sep=',', data=None):
+	def createDataObject(self, path, name, type_of_data, description='', sep=',', data=None, index_col=None):
 		'''Create a dataObj and store it in data_container'''
 		if (type_of_data != 'measurement') and (type_of_data != 'clinical') and (type_of_data != 'accessory'):
 			raise ValueError('Type can only be \'measurement\' or \'clinical\'')
+		elif name in self.data_container:
+			raise ValueError(f'''A data object named {name} is already present in the /
+			data manager, please choose a different name.''')
 		elif (path) and (data is None):
-			data = pd.read_csv(path, sep=sep)
-			self.data_container[name] = dataObj(path, name, data, type_of_data, description)
+			data = pd.read_csv(path, sep=sep, index_col=index_col)
+			indexed = isinstance(type(data.index), pd.RangeIndex)
+			self.data_container[name] = dataObj(path, name, data, type_of_data, description, is_data_index_set=indexed)
 		elif not (path) and (data is not None):
-			self.data_container[name] = dataObj(path, name, data, type_of_data, description)
+			indexed = isinstance(type(data.index), pd.RangeIndex)
+			self.data_container[name] = dataObj(path, name, data, type_of_data, description, is_data_index_set=indexed)
 		else:
 			print('''something went wrong while creating a data object, \
 					it is not possible to continue. Please check you parameters \
 					and ensure they are correct.''')
+		if not indexed:
+			print('''Your dataset doesn\'t have an index, or is index if of type /
+					pd.RangeIndex. Please note that this type of index is not supported /
+					during operation such as join. Please set a different type of index /
+					with \'setDataObjIndex\'''')
+
+	def setDataObjIndex(self, dataObj, index_col):
+		if index_col in dataObj.data.columns:
+			dataObj.data.set_index(index_col, inplace=True)
+		else:
+			raise ValueError(f'{index_col} is not in {dataObj.name} columns')
 
 	def deleteDataObject(self, *args):
 		'''Delete a dataObj from data_container'''
@@ -79,12 +96,17 @@ class dataManager:
 
 	def dataObjectJoin(self, dataObj1, dataObj2, del_parents=False, **kwargs):
 		'''Perform a join operation on 2 dataObj'''
-		data = dataObj1.join(dataObj2, kwargs)
-		name = f'joined {dataObj1.name} and {dataObj2.name}'
-		type_of_data = dataObj1.type_of_data
-		description = f'''results of join between {dataObj1.name} and /
-		{dataObj2.name} with the following parameters: {kwargs}'''
-		self.createDataObject('', name, type_of_data, description, data=data)
-		if del_parents:
-			self.deleteDataObject(dataObj1.name, dataObj2.name)
-
+		if dataObj1.is_data_index_set and dataObj2.is_data_index_set:
+			data = dataObj1.join(dataObj2, kwargs)
+			name = f'joined {dataObj1.name} and {dataObj2.name}'
+			type_of_data = dataObj1.type_of_data
+			description = f'''results of join between {dataObj1.name} and /
+			{dataObj2.name} with the following parameters: {kwargs}'''
+			self.createDataObject('', name, type_of_data, description, data=data)
+			if del_parents:
+				self.deleteDataObject(dataObj1.name, dataObj2.name)
+		else:
+			print('''Your dataset doesn\'t have an index, or is index if of type /
+					pd.RangeIndex. Please note that this type of index is not supported /
+					during operation such as join. Please set a different type of index /
+					with \'setDataObjIndex\'''')
