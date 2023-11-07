926ae0aa45efa4a9f25b4f4f3ec6a793e86b1cdd
Tiziano Dallavilla <tiziano.dallavilla@gmail.com>
2023-11-07 13:26:02 +0100

    Added functions to save and load a dataManager object
    -dataManager object can now be assigned a name (which will be used when saving the object as file name)
    -added function saveDataManager that allows to save a dataManager object
    -added function loadDataManager to load a dataManager object

--- lib/ml_feature_prediction.py
+++ lib/ml_feature_prediction.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 import pandas as pd
+import pickle
+import os
 
 
 # Class that contains data
@@ -14,8 +16,9 @@ class dataObj:
 
 
 class dataManager:
-	def __init__(self):
+	def __init__(self, name='dataManager'):
 		self.data_container = {}
+		self.dataManager_name = name
 
 	def createDataObject(self, path, name, type_of_data, description='', sep=','):
 		'''Create a dataObj and store it in data_container'''
@@ -33,7 +36,7 @@ class dataManager:
 			print(f'No data object named {name} is present in the data container!')
 
 	def printDataContainer(self, extended=False):
-		'''Print the dataObj names present in data_container, if extended = True 
+		'''Print the dataObj names present in data_container, if extended = True
 		also print path, type_of_data, and decription'''
 		for key in self.data_container:
 			print(f'Name: {key}')
@@ -42,3 +45,26 @@ class dataManager:
 				print(f'Type of data: {self.data_container[key].type_of_data}')
 				print(f'Description: {self.data_container[key].description}')
 			print('---END---')
+
+	def saveDataManager(self, folderpath):
+		'''Save the current dataManager object to the specified path'''
+		filepath = '/'.join([folderpath.rstrip('/'), filename])
+		filename = self.dataManager_name + '.obj'
+		if os.path.isdir(folderpath):  # Check if folder exist
+			if not os.path.isfile(filepath):  # Check if file already exist
+				with open(filepath, 'wb') as file:
+					pickle.dump(self, file)
+					print(f'{self.dataManager_name} successfully saved to \'{filepath}\'')
+			else:
+				print(f'A file named {filename} already exist, saving process is aborted.')
+		else:
+			print('The path specified does not exist, saving process is aborted.')
+
+	@classmethod
+	def loadDataManager(cls, filepath):
+		'''Load a dataManager objectfrom the specified path'''
+		if os.path.isfile(filepath):  # Check if file exist
+			with open(filepath, 'rb') as file:
+				return pickle.load(file)
+		else:
+			print('Can\'t import the data manager object, file does not exist.')
