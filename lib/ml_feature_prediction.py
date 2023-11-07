253cf35de9fe976e664de7e5bc37b581967353ca
Tiziano Dallavilla <tiziano.dallavilla@gmail.com>
2023-11-07 10:51:34 +0100

    Updates to dataObj and dataManager classes
    -added field name to class dataObj
    -added field description to class dataObj
    -added 'accessory' as option in dataManager 'type_of_data' field
    -created a variable named 'data_container' in dataManager class. The purpose is to store all the dataObj of a project.
    -function dataManager now add a dataObj object to variable 'data_container' instead of returning the object
    - added function 'deleteDataObject' to remove a dataObj from the data_container
    -added function 'printDataContainer' that prints all the dataObj in data_container

--- lib/ml_feature_prediction.py
+++ lib/ml_feature_prediction.py
@@ -5,15 +5,37 @@ import pandas as pd
 # Class that contains data
 @dataclass
 class dataObj:
+	''' This class is ment to store clinical and measurement data'''
 	path: str
+	name: str
 	data: pd.DataFrame()
 	type_of_data: str
+	description: str = ''
 
 
 class dataManager:
-	def createDataObject(self, path, type_of_data, sep=','):
-		if (type_of_data != 'measurement') and (type_of_data != 'clinical'):
+	def __init__(self):
+		self.data_container = {}
+
+	def createDataObject(self, path, name, type_of_data, description='', sep=','):
+		'''Create a dataObj and store it in data_container'''
+		if (type_of_data != 'measurement') and (type_of_data != 'clinical') and (type_of_data != 'accessory'):
 			raise ValueError('Type can only be \'measurement\' or \'clinical\'')
 		else:
 			data = pd.read_csv(path, sep=sep)
-			return dataObj(path, data, type_of_data)
+			self.data_container[name] = dataObj(path, name, data, type_of_data, description)
+
+	def deleteDataObject(self, name):
+		'''Delete a dataObj from data_conteiner'''
+		self.data_container.pop(name, f'No data object named {name} is present in the data container!')
+
+	def printDataContainer(self, extended=False):
+		'''Print the dataObj names present in data_container, if extended = True 
+		also print path, type_of_data, and decription'''
+		for key in self.data_container:
+			print(f'Name: {key}')
+			if extended:
+				print(f'Path: {self.data_container[key].path}')
+				print(f'Type of data: {self.data_container[key].type_of_data}')
+				print(f'Description: {self.data_container[key].description}')
+			print('---END---')
